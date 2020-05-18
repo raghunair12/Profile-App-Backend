eac92cc77b06a58802a82b62d7ff41e0cd92ed22
raghunair12 <raghu.nair99@gmail.com>
2020-05-18 09:48:11 +0530

    Implemented POST and GET APIs

--- controllers/details-controllers.js
+++ controllers/details-controllers.js
@@ -1,5 +1,15 @@
 const Detail = require('../models/details');
 
+const getAllDetails = async(req,res,next) => {
+    let details;
+    try{
+        details = await Detail.find({}, '-__v');
+    }catch(err){
+        return res.status(404).json({message: 'Data not found'});
+    }
+    res.json({details: details.map(d=>d.toObject({getters:true}))});
+}
+
 const submit = async (req,res,next) => {
     const {firstName, lastName, company, residingState, gender, favMovie, phoneNo, favApp, weight} = req.body;
     const createdDetails = new Detail({
@@ -21,4 +31,28 @@ const submit = async (req,res,next) => {
     res.status(201).json({detail: createdDetails.toObject({getters:true})});
 };
 
+const deleteDetail = async (req,res,next) =>{
+    const detailId = req.params.id;
+    let detail;
+    try{
+        detail = await Detail.findById(detailId);
+    }catch(err){
+        return res.status(400).json({message: 'Something went wrong, Could not delete'});
+    }
+    
+    if(!detail){
+        return res.status(404).json({message: 'Could find the Detail ID'});
+    }
+
+    try{
+        await detail.remove();
+    }catch(err){
+        return res.status(400).json({message: 'Could not Delete'});
+    }
+
+    res.json({message:'Deleted..'});
+}
+
+exports.getAllDetails = getAllDetails;
 exports.submit = submit;
+exports.deleteDetail= deleteDetail;
--- routes/details-routes.js
+++ routes/details-routes.js
@@ -4,6 +4,10 @@ const router = express.Router();
 
 const detailsController = require('../controllers/details-controllers');
 
+router.get('/', detailsController.getAllDetails);
+
 router.post('/submit', detailsController.submit);
+
+router.delete('/:id', detailsController.deleteDetail)
 
 module.exports = router;
